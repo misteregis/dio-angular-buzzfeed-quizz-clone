f2460a55742800f27ced4871f71599a2a90f4604
misteregis <misteregis@gmail.com>
2023-12-16 18:49:37 -0300

    I changed the quiz to quiz vector and added more quizzes

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,78 +1,82 @@
 import { Component, OnInit } from '@angular/core';
-import quizz_questions from "../../../assets/data/quizz_questions.json"
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
-  styleUrls: ['./quizz.component.css']
+  styleUrls: ['./quizz.component.css'],
 })
-
 export class QuizzComponent implements OnInit {
+  title: string = '';
 
-  title:string = ""
+  questions: any;
+  questionSelected: any;
 
-  questions:any
-  questionSelected:any
+  answers: string[] = [];
+  answerSelected: string = '';
 
-  answers:string[] = []
-  answerSelected:string =""
+  questionIndex: number = 0;
+  questionMaxIndex: number = 0;
 
-  questionIndex:number =0
-  questionMaxIndex:number=0
+  finished: boolean = false;
 
-  finished:boolean = false
+  quizz: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    if(quizz_questions){
-      this.finished = false
-      this.title = quizz_questions.title
+    if (quizz_questions) {
+      const length = quizz_questions.length;
 
-      this.questions = quizz_questions.questions
-      this.questionSelected = this.questions[this.questionIndex]
+      this.quizz = quizz_questions[this.generateRandomNumber(length)];
+      this.finished = false;
+      this.title = this.quizz.title;
 
-      this.questionIndex = 0
-      this.questionMaxIndex = this.questions.length
+      this.questions = this.quizz.questions;
+      this.questionSelected = this.questions[this.questionIndex];
 
-      console.log(this.questionIndex)
-      console.log(this.questionMaxIndex)
-    }
-
-  }
-
-  playerChoose(value:string){
-    this.answers.push(value)
-    this.nextStep()
-
-  }
-
-  async nextStep(){
-    this.questionIndex+=1
-
-    if(this.questionMaxIndex > this.questionIndex){
-        this.questionSelected = this.questions[this.questionIndex]
-    }else{
-      const finalAnswer:string = await this.checkResult(this.answers)
-      this.finished = true
-      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results ]
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
     }
   }
 
-  async checkResult(anwsers:string[]){
-
-    const result = anwsers.reduce((previous, current, i, arr)=>{
-        if(
-          arr.filter(item => item === previous).length >
-          arr.filter(item => item === current).length
-        ){
-          return previous
-        }else{
-          return current
-        }
-    })
-
-    return result
+  playerChoose(value: string) {
+    this.answers.push(value);
+    this.nextStep();
   }
 
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      const finalAnswer: string = await this.checkResult(this.answers);
+
+      this.finished = true;
+      this.answerSelected =
+        this.quizz.results[finalAnswer as keyof typeof this.quizz.results];
+    }
+  }
+
+  async checkResult(anwsers: string[]) {
+    const result = anwsers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter((item) => item === previous).length >
+        arr.filter((item) => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    });
+
+    return result;
+  }
+
+  generateRandomNumber(max: number): number {
+    const randomNumber = Math.floor(Math.random() * max);
+
+    return randomNumber;
+  }
 }
